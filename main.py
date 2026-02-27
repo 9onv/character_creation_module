211108dcf4d78c94568ed45aea54ac7052032a06
lego <lo971@yandex.ru>
2026-02-27 22:00:15 +0300

    Add docstring

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция генерирует количество очков атаки в зависимости
+    от выбранного типа персонажа
+    и возвращает строковое сообщение о проведённой атаке."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +18,10 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция генерирует количество очков защиты
+    в зависимости от выбранного типа персонажа
+    и возвращает строковое сообщение
+    о выполненном блокировании атаки."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -24,6 +31,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция в зависимости от выбранного
+    типа персонажа возвращает сообщение
+    о применении специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +46,9 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция запускает цикл тренировки навыков персонажа.
+    В качестве параметров она получает
+    введённое игроком имя персонажа и выбранный тип персонажа."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +73,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция позволяет игроку выбрать
+    тип игрового персонажа и возвращает выбранный вариант."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
